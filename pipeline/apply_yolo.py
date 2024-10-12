f4744dd9507d3b1407a04d724621a9508fa35e55
rohan.sawahn <rohan.sawahn@student.hpi.de>
2024-10-12 13:55:36 +0200

    added video output

--- pipeline/apply_yolo.py
+++ pipeline/apply_yolo.py
@@ -13,26 +13,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 from cropper import only_crop_image
 import supervision as sv
+
 fixed_size = (416, 416)
+
+
 def calculate_single_bbox(bbox, width, height):
-    # try:
-    #     logging.info(f"Detected {bbox.cls} with confidence {bbox.conf:.2f}")
-    # except:
-    #     print("Error")
-    # print(bbox)
-    box = bbox['xyxy']  # Bounding box coordinates [x1, y1, x2, y2]
+    box = bbox["xyxy"]  # Bounding box coordinates [x1, y1, x2, y2]
     x_center = (box[0] + box[2]) / 2 / width
     y_center = (box[1] + box[3]) / 2 / height
     box_width = (box[2] - box[0]) / width
     box_height = (box[3] - box[1]) / height
-    return [int(bbox['cls']), x_center, y_center, box_width, box_height]
+    return [int(bbox["cls"]), x_center, y_center, box_width, box_height]
+
+
 def process_image(image_path, model, detection_model, confidence, vid_stride):
     tracks = {}
     cap = cv2.VideoCapture(str(image_path))
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     logging.info(f"Processing video {image_path} with {length} frames.")
     i = 0
-    
 
     while cap.isOpened():
         success, frame = cap.read()
@@ -47,16 +46,17 @@ def process_image(image_path, model, detection_model, confidence, vid_stride):
         if success:
             # Convert frame to PIL Image for MegaDetector
             img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            if img.mode == 'RGBA':
-                img = img.convert('RGB')
+            if img.mode == "RGBA":
+                img = img.convert("RGB")
             np_img = np.array(img.convert("RGB"))
-            transform = pw_trans.MegaDetector_v5_Transform(target_size=detection_model.IMAGE_SIZE,
-                                               stride=detection_model.STRIDE)
-            detection_result = detection_model.single_image_detection(transform(np_img), np_img.shape)
-            detections = detection_result['detections']
-            print(detection_result)
-            # detection_result['img_id'] = 'test.png'
-            # pw_utils.save_detection_images(detection_result, "./demo_output")
+            transform = pw_trans.MegaDetector_v5_Transform(
+                target_size=detection_model.IMAGE_SIZE, stride=detection_model.STRIDE
+            )
+            detection_result = detection_model.single_image_detection(
+                transform(np_img), np_img.shape
+            )
+            detections = detection_result["detections"]
+
             # Extract gorilla bounding boxes from MegaDetector detections
             gorilla_boxes = []
             for idx, class_id in enumerate(detections.class_id):
@@ -70,50 +70,72 @@ def process_image(image_path, model, detection_model, confidence, vid_stride):
             # Crop the frame based on the bounding boxes and run YOLOv8 on each crop
             if len(gorilla_boxes) == 0:
                 continue
-            print(gorilla_boxes)
             for box in gorilla_boxes:
                 # cropped_img = only_crop_image(img, *box)
-                cropped_img = sv.crop_image(
-                    image=np_img, xyxy=box
-                    )
+                cropped_img = sv.crop_image(image=np_img, xyxy=box)
 
-                # cropped_img = only_crop_image(img, *box)
-                #x1, y1, x2, y2 = map(int, box)
-                # cropped_frame = frame[y1:y2, x1:x2]
-                #cropped_tensor = F.crop(img_tensor, top=y1, left=x1, height=y2 - y1, width=x2 - x1)
-                
-                # image_np = cropped_tensor.permute(1, 2, 0).numpy()
-                # plt.imshow(image_np)
-                # plt.axis('off')  # Turn off axis for better visualization
-                # plt.show()
-                #cropped_frame = cropped_tensor[0].permute(1, 2, 0).numpy()
-                #cv2.imwrite("/Users/lukaslaskowski/Documents/HPI/gorillavision/gorillavision-interspecies-money/image.png", cropped_frame)
-                #cv2.imwrite("/Users/lukaslaskowski/Documents/HPI/gorillavision/gorillavision-interspecies-money/image.png", cropped_frame)
                 # Run YOLOv8 tracking on the cropped frame
                 cropped_frame = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB)
                 cropped_frame = cv2.resize(cropped_frame, (416, 416))
-                results = model.track(cropped_frame, persist=True, conf=float(confidence), vid_stride=int(vid_stride),device=0, iou=0.2)
+                results = model.track(
+                    cropped_frame,
+                    persist=True,
+                    conf=float(confidence),
+                    vid_stride=int(vid_stride),
+                    device=0,
+                    iou=0.2,
+                )
                 nothing_in_track = False
                 if results[0].boxes.is_track is False:
                     nothing_in_track = True
-                    results = model.predict(cropped_frame, conf=float(confidence), vid_stride=int(vid_stride))
+                    results = model.predict(
+                        cropped_frame,
+                        conf=float(confidence),
+                        vid_stride=int(vid_stride),
+                    )
                     gorilla_detected_in_predict = len(results[0].boxes.cls) > 0
-                    print("Nothing in track, predicting with predict function", gorilla_detected_in_predict)
+                    print(
+                        "Nothing in track, predicting with predict function",
+                        gorilla_detected_in_predict,
+                    )
 
                 for result in results:
                     if result.boxes.id is not None:
                         for idx, id in enumerate(result.boxes.id):
-                            id = "no_track" if nothing_in_track and gorilla_detected_in_predict else str(int(id.item()))
-                            print("JJJJ", id == "no_track")
+                            id = (
+                                "no_track"
+                                if nothing_in_track and gorilla_detected_in_predict
+                                else str(int(id.item()))
+                            )
                             if id not in tracks:
                                 tracks[id] = {}
-                                tracks[id]['result'] = []
-                            tracks[id]['result'].append({'xyxy': result.boxes.xyxy[idx].tolist(), 'img': result.orig_img, 'cls': result.boxes.cls[idx].item()})
+                                tracks[id]["result"] = []
+
+                            xyxy_face_in_body_crop = result.boxes.xyxy[idx].tolist()
+                            xyxy_body_crop_in_full_frame = box
+                            xyxy_face_in_full_frame = np.add(
+                                xyxy_face_in_body_crop, xyxy_body_crop_in_full_frame
+                            )
+                            tracks[id]["result"].append(
+                                {
+                                    "xyxy": xyxy_face_in_full_frame,
+                                    "img": np_img,
+                                    "cls": result.boxes.cls[idx].item(),
+                                    "frame_id": i,
+                                }
+                            )
         else:
             break
+
     for key in tracks.keys():
-        tracks[key]['bboxes'] = [] #calculate_bbox(tracks[result]['result'])
-        for result in tracks[key]['result']:
-            tracks[key]['bboxes'].append(calculate_single_bbox(result, Image.fromarray(result['img'], "RGB").size[0], Image.fromarray(result['img'], "RGB").size[1]))
+        tracks[key]["bboxes"] = []  # calculate_bbox(tracks[result]['result'])
+        for result in tracks[key]["result"]:
+            tracks[key]["bboxes"].append(
+                calculate_single_bbox(
+                    result,
+                    Image.fromarray(result["img"], "RGB").size[0],
+                    Image.fromarray(result["img"], "RGB").size[1],
+                )
+            )
     cap.release()
     return tracks
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,6 +5,7 @@ import os
 import shutil
 from pathlib import Path
 
+import cv2
 from ultralytics import YOLO
 from PIL import Image
 
@@ -13,20 +14,41 @@ from cropper import crop_image
 from image_service import ImageService
 from PytorchWildlife.models import detection as pw_detection
 
+
 # from apply_dino import Dino, make_classification_eval_transform
 
+
 class Pipeline:
-    def __init__(self, name, input_dataset_path, output_path, reid_params, yolo_path, prediction_params, allowed_extensions):
+    def __init__(
+        self,
+        name,
+        input_dataset_path,
+        output_path,
+        reid_params,
+        yolo_path,
+        prediction_params,
+        allowed_extensions,
+        save_output_videos=False,
+    ):
         self.name = name
         self.input_dataset_path = input_dataset_path
         self.output_path = output_path
+        self.save_output_videos = save_output_videos
         logging.info(f"Loading yolo model from: {Path(yolo_path)}")
         self.yolo = YOLO(yolo_path)
-        self.detection_model = pw_detection.MegaDetectorV5(device="cuda", pretrained=True) # Model weights are automatically downloaded.
-        self.yolo.to('cuda')
+        self.detection_model = pw_detection.MegaDetectorV5(
+            device="cuda", pretrained=True
+        )  # Model weights are automatically downloaded.
+        self.yolo.to("cuda")
         self.prediction_params = prediction_params
         # self.reid_model = Dino(model_path=reid_params["model_path"], 'cuda' if torch.cuda.is_available() else 'cpu')
-        self.image_service = ImageService(input_dataset_path, output_path, allowed_extensions["allowed_img_extensions"], allowed_extensions["allowed_video_extensions"])
+        self.image_service = ImageService(
+            input_dataset_path,
+            output_path,
+            allowed_extensions["allowed_img_extensions"],
+            allowed_extensions["allowed_video_extensions"],
+            self.save_output_videos,
+        )
 
     def run(self):
         logging.info("Detecting faces using Yolo.")
@@ -34,34 +56,85 @@ class Pipeline:
             bboxes, images = self.detect_face(img)
             paths = self.crop_to_face(bboxes, images, img)
             # for path in paths:
-            #     img = Image.open(path).convert("RGB")
+            #     img = Image.open(path).convert("RGB")9
             #     img = make_classification_eval_transform()(img)
             #     s = self.reid_model(img.unsqueeze(0))
             #     print("PREDICTIOn", s)
             #     # apply dino
-        l = map(lambda x: 
-                    self.crop_to_face(x, self.detect_face(x)), self.image_service)
         logging.info("Done so far.")
 
     def detect_face(self, img):
         label_path = os.path.join(self.output_path, "labels", img[1])
+        output_video = None
         os.makedirs(label_path, exist_ok=True)
-        results = process_image(img[0], self.yolo, self.detection_model, self.prediction_params["confidence"], self.prediction_params["vid_stride"])
+        detected_tracks = process_image(
+            img[0],
+            self.yolo,
+            self.detection_model,
+            self.prediction_params["confidence"],
+            self.prediction_params["vid_stride"],
+        )
+
         image_bboxes = []
-        for key, images in results.items():
-            print("aaw", key, len(images))
-            print(type(images))
-            for idx, bbox in enumerate(images['bboxes']):
-                label = img[0].stem + f"_id_{key}" if len(images['result']) == 1 else img[0].stem + f"_id_{key}" + f"_{idx}"
+        for key, images in detected_tracks.items():
+            for idx, bbox in enumerate(images["bboxes"]):
+                label = (
+                    img[0].stem + f"_id_{key}"
+                    if len(images["result"]) == 1
+                    else img[0].stem + f"_id_{key}" + f"_{idx}"
+                )
                 local_label_path = os.path.join(label_path, label + ".txt")
-                with open(local_label_path, 'w') as f:
+                with open(local_label_path, "w") as f:
                     line = f"{bbox[0]} {bbox[1]} {bbox[2]} {bbox[3]} {bbox[4]}"
                     f.write("%s\n" % line)
-                image_bboxes.append({'label': label, 'bboxes':bbox})
+                image_bboxes.append({"label": label, "bboxes": bbox})
+
         output = []
-        for key, result in results.items():
-            for res in result['result']:
+        for key, result in detected_tracks.items():
+            for res in result["result"]:
                 output.append(res)
+
+        # collect all the boxes per frame and save them as a video
+        bboxes_per_frame = {}
+        for key, images in detected_tracks.items():
+            for idx, res in enumerate(images["results"]):
+                frame_id = res["frame_id"]
+                if not frame_id in bboxes_per_frame:
+                    bboxes_per_frame[frame_id] = {"img": res["img"], "bboxes": []}
+                bboxes_per_frame[frame_id]["bboxes"].append(res["bboxes"])
+
+        # save the video with the boxes
+
+        input_path = Path(img[0])
+        if (
+            self.save_output_videos
+            and input_path.suffix.lower()
+            in self.allowed_extensions["allowed_video_extensions"]
+        ):
+            video_output_path = os.path.join(self.output_path, "video_results", img[1])
+            os.makedirs(video_output_path, exist_ok=True)
+            cap = cv2.VideoCapture(str(input_path))
+            frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            output_video = cv2.VideoWriter(
+                os.path.join(video_output_path, img[1] + "_output.mp4"),
+                cv2.VideoWriter_fourcc(*"mp4v"),
+                fps,
+                (frame_width, frame_height),
+            )
+
+            for frame_id, frame in bboxes_per_frame.items():
+                for bboxes in frame["bboxes"]:
+                    for bbox in bboxes:
+                        x1, y1, x2, y2 = bbox
+                        cv2.rectangle(frame["img"], (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    output_video.write(frame["img"])
+
+            output_video.release()
+            cap.release()
+
         return image_bboxes, output
 
     def crop_to_face(self, image_bboxes, images, img):
@@ -72,9 +145,9 @@ class Pipeline:
             if len(bboxes) == 0:
                 logging.warning(f"No bboxes found for {img[0].stem}")
                 continue
-            local_cropped_path = os.path.join(cropped_path, bboxes['label'] + ".png")
-            #local_cropped_path = os.path.join(cropped_path, img[0].stem + ".png") if len(images) == 1 else os.path.join(cropped_path, img[0].stem + f"_{idx}.png")
-            cropped_image = crop_image(image, bboxes['bboxes'])
+            local_cropped_path = os.path.join(cropped_path, bboxes["label"] + ".png")
+            # local_cropped_path = os.path.join(cropped_path, img[0].stem + ".png") if len(images) == 1 else os.path.join(cropped_path, img[0].stem + f"_{idx}.png")
+            cropped_image = crop_image(image, bboxes["bboxes"])
             cropped_image.save(local_cropped_path)
             cropped_paths.append(local_cropped_path)
         return cropped_paths
@@ -85,28 +158,35 @@ class Pipeline:
     def calculate_embeddings(self):
         raise NotImplementedError
 
-
     def fiftyone(self):
         raise NotImplementedError
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("-n", "--name", required=True, help="Name of the dataset")
-    ap.add_argument("-d", "--dataset-path", required=True, help="Path to the image folder")
+    ap.add_argument(
+        "-d", "--dataset-path", required=True, help="Path to the image folder"
+    )
     ap.add_argument("-o", "--output-path", required=True, help="Path to the new folder")
     args = vars(ap.parse_args())
 
     config = configparser.ConfigParser()
-    config.read('config.ini')
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    config.read("config.ini")
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
     logging.info("Pipeline started")
     pipeline = Pipeline(
-        name=args['name'],
-        input_dataset_path=args['dataset_path'],
+        name=args["name"],
+        input_dataset_path=args["dataset_path"],
         prediction_params=config["PREDICTION"],
-        reid_params = config["REID"],
-        output_path = args["output_path"], 
-        yolo_path=config['DEFAULT']['yolo_path'],
-        allowed_extensions={'allowed_img_extensions': config['DEFAULT']['allowed_img_extensions'], 'allowed_video_extensions': config['DEFAULT']['allowed_video_extensions']}
+        reid_params=config["REID"],
+        output_path=args["output_path"],
+        yolo_path=config["DEFAULT"]["yolo_path"],
+        allowed_extensions={
+            "allowed_img_extensions": config["DEFAULT"]["allowed_img_extensions"],
+            "allowed_video_extensions": config["DEFAULT"]["allowed_video_extensions"],
+        },
     )
     pipeline.run()
